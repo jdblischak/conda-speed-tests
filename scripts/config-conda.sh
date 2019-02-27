327f223dc944a8e01d46403212bcb561e7983f62
John Blischak <jdblischak@gmail.com>
2019-02-27 14:56:25 -0500

    Refactor setup into scripts.

---
+++ scripts/config-conda.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eux
+
+# Configure conda
+
+conda config --set always_yes yes
+conda config --set changeps1 no
+conda config --set auto_update_conda no
+
---
+++ scripts/install-system.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eux
+
+# Install system dependencies
+
+apt-get update
+apt-get install --yes --no-install-recommends \
+  time
+
