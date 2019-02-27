35f45bcdc5e96f86c10ce39e1d269b42d72a5246
John Blischak <jdblischak@gmail.com>
2019-02-27 17:07:16 -0500

    Test installation of r-workflowr from jdblischak.

---
+++ scripts/test-r-workflowr-jdblischak.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -eux
+
+# Install r-workflowr from jdblischak
+
+conda config --add channels jdblischak
+conda config --add channels defaults
+conda config --add channels conda-forge
+conda create --quiet -n r-workflowr-jdb python
+
+mkdir -p list/ log/ time/
+
+/usr/bin/time -f "%e %U %S %P %M" -o time/r-workflowr-jdb-${VER}.txt \
+  conda install --quiet -vv -n r-workflowr-jdb r-workflowr=0.11.0 \
+  2> log/r-workflowr-jdb-${VER}.txt
+
+conda list -n r-workflowr-jdb > list/r-workflowr-jdb-${VER}.txt
+
+# Cleanup
+conda env remove -n r-workflowr-jdb
+conda clean --all
+conda config --remove-key channels
+
