c1960ee9e57eeee3e77b3e5eb314d982798b8521
John Blischak <jdblischak@gmail.com>
2019-02-27 16:15:43 -0500

    Put test into a script to facilitate setup/teardown.

---
+++ scripts/test-numpy-defaults.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -eux
+
+# Install numpy from defaults
+
+conda config --add channels defaults
+conda create -n numpy-defaults python
+
+mkdir -p list/ log/ time/
+
+/usr/bin/time -f "%e %U %S %P %M" -o time/numpy-defaults-${VER}.txt \
+  conda install -vv -n numpy-defaults numpy \
+  2> log/numpy-defaults-${VER}.txt
+
+conda list -n numpy-defaults > list/numpy-defaults-${VER}.txt
+
+# Cleanup
+conda env remove -n numpy-defaults
+conda clean --all
+conda config --remove-key channels
+
