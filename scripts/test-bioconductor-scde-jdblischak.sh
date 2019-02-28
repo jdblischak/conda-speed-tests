a23733db9451194af93f765d11a7d54abc25108b
John Blischak <jdblischak@gmail.com>
2019-02-27 22:12:37 -0500

    Test installation of bioconductor-scde from personal channel.

---
+++ scripts/test-bioconductor-scde-jdblischak.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -eux
+
+# Install bioconductor-scde from jdblischak
+
+conda config --add channels jdblischak
+conda config --add channels defaults
+conda config --add channels bioconda
+conda config --add channels conda-forge
+conda create --quiet -n scde python
+
+mkdir -p list/ log/ time/
+
+/usr/bin/time -f "%e %U %S %P %M" -o time/scde-${VER}.txt \
+  conda install --quiet -vv -n scde bioconductor-scde=1.99.1 \
+  2> log/scde-${VER}.txt
+
+conda list -n scde > list/scde-${VER}.txt
+
+# Cleanup
+conda env remove -n scde
+conda clean --all
+conda config --remove-key channels
+
